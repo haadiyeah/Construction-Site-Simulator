bab3666123a385a6afc3afab3f5e45ea922e225a
omer <i211105@nu.edu.pk>
2023-12-09 23:18:44 +0500

    Added Feasibility and Looping through Vectors

--- TasksScheduler.h
+++ TasksScheduler.h
@@ -11,69 +11,89 @@ using namespace std;
 
 class TasksScheduler
 {
-    private:
-        vector<Task> tasks;
-        vector<Task> lowPriorityTasks;
-        vector<Task> mediumPriorityTasks;
-        vector<Task> highPriorityTasks;
+private:
+    vector<Task> tasks;
+    vector<Task> lowPriorityTasks;
+    vector<Task> mediumPriorityTasks;
+    vector<Task> highPriorityTasks;
 
-    public:
-        void scheduleTask(Task& task){
+public:
+    void scheduleTask(Task &task)
+    {
 
-            // priority 1 is high
-            // priority 2 is medium
-            // priority 3 is low
+        // priority 1 is high
+        // priority 2 is medium
+        // priority 3 is low
 
-            tasks.push_back(task);
-            switch (task.priority)
+        tasks.push_back(task);
+        switch (task.priority)
+        {
+        case 1:
+            highPriorityTasks.push_back(task);
+            break;
+        case 2:
+            mediumPriorityTasks.push_back(task);
+            break;
+        case 3:
+            lowPriorityTasks.push_back(task);
+            break;
+        }
+    }
+
+    Task getTask(bool isRaining, vector<vector<Resource>> availableMaterials, vector<Worker> availableWorkers)
+    {
+
+        /*
+
+        This function returns a Task based on the priority, weather, human and materials.
+        There are 3 lists of tasks for each priority.
+
+        Indoor tasks returned if isRaining == true
+        */
+
+        // need to change this to multilevel feedback queue
+
+        // need to add task return based on external variables
+
+        for (auto iterate = highPriorityTasks.begin(); iterate != highPriorityTasks.end(); ++iterate)
+        {
+            if (isTaskFeasible(*iterate, isRaining, availableMaterials, availableWorkers))
             {
-            case 1:
-                highPriorityTasks.push_back(task);
-                break;
-            case 2:
-                mediumPriorityTasks.push_back(task);
-                break;
-            case 3:
-                lowPriorityTasks.push_back(task);
-                break;
+                Task feasibleTask = *iterate;
+                highPriorityTasks.erase(iterate);
+                return feasibleTask;
             }
         }
 
-        Task getTask(bool isRaining, vector<vector<Resource>> availableMaterials, int availableWorkers){
-            
-            /* 
-            
-            This function returns a Task based on the priority, weather, human and materials.
-            There are 3 lists of tasks for each priority.
-
-            Indoor tasks returned if isRaining == true
-            Task end of queue if Material low
-            Task end of queue if Worker low
-
-            */
-
-           // need to change this to priority
-
-           // need to add task return based on external variables
-            Task selectTask;
-            // getting tasks from arrays            
-            if (!highPriorityTasks.empty()) {
-                selectTask = highPriorityTasks.front();
-                highPriorityTasks.erase(highPriorityTasks.begin());
-            }
-            else if (!mediumPriorityTasks.empty())
+        for (auto iterate = mediumPriorityTasks.begin(); iterate != mediumPriorityTasks.end(); ++iterate)
+        {
+            if (isTaskFeasible(*iterate, isRaining, availableMaterials, availableWorkers))
             {
-                selectTask = mediumPriorityTasks.front();
-                mediumPriorityTasks.erase(mediumPriorityTasks.begin());
+                Task feasibleTask = *iterate;
+                mediumPriorityTasks.erase(iterate);
+                return feasibleTask;
             }
-            else if (!lowPriorityTasks.empty())
-            {
-                selectTask = lowPriorityTasks.front();
-                lowPriorityTasks.erase(lowPriorityTasks.begin());
-            }
-
-            return selectTask;
-
         }
 
+        for (auto iterate = lowPriorityTasks.begin(); iterate != lowPriorityTasks.end(); ++iterate)
+        {
+            if (isTaskFeasible(*iterate, isRaining, availableMaterials, availableWorkers))
+            {
+                Task feasibleTask = *iterate;
+                lowPriorityTasks.erase(iterate);
+                return feasibleTask;
+            }
+        }
+    }
+
+    bool isTaskFeasible(Task &task, bool isRaining, const vector<vector<Resource>> &availableMaterials, vector<Worker> availableWorkers)
+    {
+        if (isRaining && !task.indoor)
+        {
+            // Outdoor task become nonfeasible
+            return false;
+        }
+
+        return true;
+    }
 };
