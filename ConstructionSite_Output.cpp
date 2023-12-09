4b4230b280ffa6944434594703460d99dced50ca
musa <musaharoon.2003@gmail.com>
2023-12-09 18:20:10 +0500

    suply and degrade

--- ConstructionSite_Output.cpp
+++ ConstructionSite_Output.cpp
@@ -7,94 +7,72 @@
 #include <vector>
 #include "Resources.h"
 
-
 using namespace std;
 
 bool isRunning = true;
 const int MAX_CAPACITY =50; //max capacity for each type of resource
-pthread_mutex_t bricksMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t cementMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t toolsMutex = PTHREAD_MUTEX_INITIALIZER;
 
-queue<Resource> bricks;
-queue<Resource> cement;
-queue<Resource> tools;
+pthread_mutex_t materialsMutex[3] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
+vector<vector<Resource>> materials(3);  // 0 - bricks, 1 - cement, 2 - tools
 
 void* supplyFactory(void* arg) {
     while (isRunning) {
-        if(bricks.size() < MAX_CAPACITY) {
-            pthread_mutex_lock(&bricksMutex);
-            Resource brick;
-            brick.type = "Brick";
-            brick.quality = 100;
-            cout<<"Supply factory: Produced 1 brick "<<endl;
-            bricks.push(brick);
-            pthread_mutex_unlock(&bricksMutex);
+        for(int i = 0; i < 3; i++) {
+            if(materials[i].size() < MAX_CAPACITY) {
+                pthread_mutex_lock(&materialsMutex[i]);
+                Resource r;
+                switch(i) {
+                    case 0:
+                        r.type = "Brick";
+                        break;
+                    case 1:
+                        r.type = "Cement";
+                        break;
+                    case 2:
+                        r.type = "Tool";
+                        break;
+                }
+                r.quality = 100;
+                cout<<"Supply factory: Produced 1 "<<r.type<<endl;
+                materials[i].push_back(r);
+                pthread_mutex_unlock(&materialsMutex[i]);
+            }
         }
-        if(cement.size() < MAX_CAPACITY) {
-            pthread_mutex_lock(&cementMutex);
-            Resource c;
-            c.type = "Cement";
-            c.quality = 100;
-            cout<<"Supply factory: Produced 1 cement "<<endl;
-            cement.push(c);
-            pthread_mutex_unlock(&cementMutex);
-        }
-        if(tools.size() < MAX_CAPACITY) {
-            pthread_mutex_lock(&toolsMutex);
-            Resource t;
-            t.type = "Tool";
-            t.quality = 100;
-            cout<<"Supply factory: Produced 1 tool "<<endl;
-            tools.push(t);
-            pthread_mutex_unlock(&toolsMutex);
-        }
+        sleep(5); // supply after every 5 seconds
     }
     pthread_exit(NULL);
 }
 
-void* materialDegredation(void* arg) {
+void* materialDegredation(void* arg) { // degrades the first pushed resource of each type
     while(isRunning) {
-        if(!bricks.empty()) {
-            pthread_mutex_lock(&bricksMutex);
-            Resource brick = bricks.front();
-            bricks.pop();
-            brick.quality -= 10;
-            if(brick.quality > 0) {
-                bricks.push(brick);
+        cout << "Material Degredation: Degredation started" << endl;
+        for(int i = 0; i < 3; i++) {
+            if(!materials[i].empty()) {
+                pthread_mutex_lock(&materialsMutex[i]);
+                Resource r = materials[i].front();
+                // materials[i].pop_back(); // remove the first pushed resource
+                r.quality -= 10;
+                cout << "Material Degredation: " << r.type << " quality is " << r.quality << endl;
+                if(r.quality > 0) {
+                    // materials[i].push_back(r);
+                }
+                else {
+                    switch(i) {
+                        case 0:
+                            cout<<"Material Degredation: Brick quality is 0 and brick is destroyed"<<endl;
+                            break;
+                        case 1:
+                            cout<<"Material Degredation: Cement quality is 0 and cement is destroyed"<<endl;
+                            break;
+                        case 2:
+                            cout<<"Material Degredation: Tool quality is 0 and tool is destroyed"<<endl;
+                            break;
+                    }
+                }
+                pthread_mutex_unlock(&materialsMutex[i]);
             }
-            else {
-                cout<<"Material Degredation: Brick quality is 0 and brick is destoryed "<<endl;
-            }
-            pthread_mutex_unlock(&bricksMutex);
         }
-        if(!cement.empty()) {
-            pthread_mutex_lock(&cementMutex);
-            Resource c = cement.front();
-            cement.pop();
-            c.quality -= 10;
-            if(c.quality > 0) {
-                cement.push(c);
-            }
-            else {
-                cout<<"Material Degredation: Cement quality is 0 and cement is destroyed"<<endl;
-            }
-            pthread_mutex_unlock(&cementMutex);
-        }
-        if(!tools.empty()) {
-            pthread_mutex_lock(&toolsMutex);
-            Resource t = tools.front();
-            tools.pop();
-            t.quality -= 10;
-            if(t.quality > 0) {
-                tools.push(t);
-            }
-            else {
-                cout<<"Material Degredation: Tool quality is 0 and tool is destroyed"<<endl;
-            }
-            pthread_mutex_unlock(&toolsMutex);
-        }
-        //sleep(10); //degredation after every 10 seconds
+        sleep(8); //degredation after every 10 seconds
     }
     pthread_exit(NULL);
 }
@@ -102,11 +80,12 @@ void* materialDegredation(void* arg) {
 int main() {
 
 cout<<"Main"<<endl;
-    pthread_t tid, tid2;
-    pthread_create(&tid, NULL, supplyFactory, NULL);
-    pthread_create(&tid2, NULL, materialDegredation, NULL);
+    pthread_t supply, degrade;
+    pthread_create(&supply, NULL, supplyFactory, NULL);
+    pthread_create(&degrade, NULL, materialDegredation, NULL);
 
-    pthread_join(tid, NULL);
+    pthread_join(supply, NULL);
+    pthread_join(degrade, NULL);
 
 
     return 0;
